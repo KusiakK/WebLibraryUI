0aaada10b27b77f8f2a0447af1076fac94830ba2
blysku <blysku@DESKTOP-Debian.lan>
2018-10-01 12:30:21 +0200

    shared module, and header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
-import { AuthorService } from "./services/author.service";
+import {AuthorService} from "./services/author.service";
 import {HomeComponent} from "./home/home.component";
+import {HeaderComponent} from './shared/layout/header.component';
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    SharedModule
   ],
   providers: [AuthorService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
